55303bec0e3eda489046d1e8235e311a0884524f
my_pc <vivek.tantransh@gmail.com>
2022-08-06 18:41:52 +0530

    dao

--- app/src/main/java/com/example/mvvmarchitecture/Note.kt
+++ app/src/main/java/com/example/mvvmarchitecture/Note.kt
@@ -1,14 +1,13 @@
 package com.example.mvvmarchitecture
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 
 @Entity(tableName = "notes_table")
 class Note(@ColumnInfo(name = "text") val text: String) {
 
     @PrimaryKey(autoGenerate = true)
     var id = 0
+
 }
 
 //token
